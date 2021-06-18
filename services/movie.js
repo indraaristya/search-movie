86d95866618ab203cb6ac82f22e3e482bc79530d
Indra Aristya <putuindra@kata.ai>
2021-06-18 10:11:34 +0800

    provide env for base url & key

--- services/movie.js
+++ services/movie.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
+const BASE_URL = process.env.BASE_URL || ''
+const API_KEY = process.env.API_KEY || ''
 
 async function searchMovieByTitle(title, page = 1){
     try {
-        const response = await axios.get(`http://www.omdbapi.com/?apikey=faf7e5bb&s=${title}&page=${page}`);
+        const response = await axios.get(`${BASE_URL}/?apikey=${API_KEY}&s=${title}&page=${page}`);
         if (response.data.Response && response.data.Response == "True") {
             return {
                 data: response.data.Search,
                 count: response.data.Search.length,
-                page,
+                page: parseInt(page),
                 totalPage: Math.ceil(response.data.totalResults/10)
             }
         } else {
