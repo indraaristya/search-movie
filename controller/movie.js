1e1bd3b96a422d06b460966ce4112d3ce55d7b40
Indra Aristya <putuindra@kata.ai>
2021-06-18 11:36:55 +0800

    controller handler get movie details

--- controller/movie.js
+++ controller/movie.js
@@ -29,4 +29,35 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+router.get('/detail', async function(req, res) {
+  try {
+    const id = req.query.id;
+    const title = req.query.title;
+    let result;
+    if (id) {
+      result = await movieServices.getMovieDetailsById(id);
+    } else if (title) {
+      result = await movieServices.getMovieDetailsByTitle(title);
+    } else {
+      res.status(400);
+      res.json({
+          error: "Please provide the ID or title of the movie"
+      });
+    }
+
+    if (result.error) {
+      res.status(400);
+    } else {
+      res.status(200);
+    }
+    res.json(result);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500);
+    res.json({
+      error: "Can't retrieve movie details"
+    })
+  }
+});
+
 module.exports = router;
